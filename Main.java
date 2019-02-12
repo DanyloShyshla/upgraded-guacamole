380a03e9f929cb77638bb75ef2a43789e4c0b031
Danylo <dshyshla@gmail.com>
2019-02-12 09:49:36 +0200

    Realization of classes and access methods in Java

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Saw sawFirst = new Saw("Still AKE-30 S", 1200, 8, 3, 2999);
         Saw sawSecond = new Saw();
-        Saw sawThird = new Saw("Still AB-42 m", 2000, 12, 5, 6049);
+        Saw sawThird = new Saw("Still AB-42 m", 2000, 12, 5, 6049, 7, 4);
         System.out.println(sawFirst);
         System.out.println(sawSecond);
         System.out.println(sawThird);
